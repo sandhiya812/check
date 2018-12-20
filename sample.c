bc7338ca045bbcbc6c988fe330b12eebc661d54a
sandhiya812 <43338242+sandhiya812@users.noreply.github.com>
2018-12-20 14:15:21 +0530

    Create sample.c

---
+++ sample.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char c;
+int islowercasevowel,isuppercasevowel;
+printf("%c",&c);
+islowercasevowel=(c=='a'//c=='e'//c=='i'//c=='o'//c=='u');
+isuppercasevowel=(c=='A'//c=='E'//c=='I'//c=='O'//c=='U');
+if(islowercasevowel//isuppercasevowel)
+printf("%c is a vowel.",c);
+else
+printf("%c is a consonant.",c);
+return 0;
+:
